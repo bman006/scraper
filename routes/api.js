0fd0a1af77739a9f6d2a0d47324b55338eab817f
Brian Parkes <parkesbrianj@gmail.com>
2018-04-02 18:18:31 -0500

    working on server stuff still

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,21 @@
-var api = '';
+var express = require('express');
+var router = express.Router();
+const cheerio = require('cheerio');
+var request = require('request');
 
-module.exports = api;
+// Scrape data
+router.get('/scrape', function() {
+    // Web page to scrape data from
+    var pageToScrape = 'https://www.allmusic.com/';
+    // Variable to store page html in
+    var html = "";
+
+    // Get html
+    request(pageToScrape, function (error, response, body) {
+        console.log('Request error:', error); // Print the error if one occurred
+        console.log('Request statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        html = body;
+    });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,18 +3,22 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cheerio = require('cheerio');
-var request = require('request');
+
 
 // Require models for database
 var db = require("./models");
 
-// Require routes
-var router = require("./routes");
-
 // Setup express
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// Create static directory for public files
+app.use(express.static("public"));
+console.log('got hererererererer');
+
+// Require routes
+var routes = require("./routes");
+app.use(routes);
 
 // Connect to Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scraperdb'
@@ -22,24 +26,10 @@ mongoose.connect(MONGODB_URI, {
     useMongoClient: true
 });
 
-// Create static directory for public files
-app.use(express.static("public"));
 
 // Set up body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Web page to scrape data from
-var pageToScrape = 'https://www.allmusic.com/';
-// Variable to store page html in
-var html = "";
-
-// Get html
-request(pageToScrape, function (error, response, body) {
-    console.log('Request error:', error); // Print the error if one occurred
-    console.log('Request statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    html = body;
-});
 
 const $ = cheerio.load(html);
 
