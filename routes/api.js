a84c7dcfce4b8b27da8b7a5bc2971cab6a40f18d
Brian Parkes <parkesbrianj@gmail.com>
2018-04-03 20:24:38 -0500

    Scraper pulling data successfully

--- routes/api.js
+++ routes/api.js
@@ -5,17 +5,17 @@ var request = require('request');
 
 // Scrape data
 router.get('/scrape', function() {
+    console.log('Called');
     // Web page to scrape data from
     var pageToScrape = 'https://www.allmusic.com/';
-    // Variable to store page html in
-    var html = "";
 
     // Get html
-    request(pageToScrape, function (error, response, body) {
+    request(pageToScrape, function (error, response, html) {
         console.log('Request error:', error); // Print the error if one occurred
         console.log('Request statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        html = body;
+        const $ = cheerio.load(html);
+        res.render(html);
     });
 })
 
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var path = require('path');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+var request = require('request');
 const mongoose = require('mongoose');
 
 
@@ -14,11 +16,10 @@ var PORT = process.env.PORT || 3000;
 
 // Create static directory for public files
 app.use(express.static("public"));
-console.log('got hererererererer');
 
 // Require routes
-var routes = require("./routes");
-app.use(routes);
+require("./routes");
+// app.use(routes);
 
 // Connect to Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scraperdb'
@@ -31,10 +32,40 @@ mongoose.connect(MONGODB_URI, {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const $ = cheerio.load(html);
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Routes
+
+// Scrape data from site
+app.get('/scrape', function() {
+    // Web page to scrape data from
+    var pageToScrape = 'https://www.allmusic.com/';
+
+    // Get html
+    request(pageToScrape, function (error, res, html) {
+        console.log('Request error:', error); // Print the error if one occurred
+        console.log('Request statusCode:', res && res.statusCode); // Print the response status code if a response was received
+        const $ = cheerio.load(html);
+
+        // Save info from html (headline, summary, url)
+        var results = [];
+
+        $('.release-container').each(function(i, element) {
+            var album = $(element).find('div.title').children('a').text();
+            results.push({
+                album: album
+            })
+        });
+        console.log(results);
+        // Save data to MongoDB
+
+    });
+})
+
+
+
+
 
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
